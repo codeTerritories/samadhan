028022b552a0535338064d62ff49db3ae915d13e
Aditya Dubey <ad5257649@gmail.com>
2026-04-20 10:58:58 +0530

    anaytics installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './styles/globals.css'
@@ -7,5 +8,6 @@ import './styles/globals.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 )
